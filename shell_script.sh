9462eae4a386e344e44025f7547047bbf0683fc9
Sachin Makavana <sachinmakavana@gmail.com>
2025-09-20 05:07:20 +0000

    Added shell_script.sh

---
+++ shell_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+VM1_PUBLIC_IP="$1"
+VM2_PRIVATE_IP="$2"
+SSH_KEY="$3"
+
+echo "Connecting to VM1 at $VM1_PUBLIC_IP..."
+ssh -o StrictHostKeyChecking=no -i $SSH_KEY azureuser@$VM1_PUBLIC_IP "ping -c 4 $VM2_PRIVATE_IP"
