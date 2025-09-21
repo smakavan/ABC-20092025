7a4aa65b19aaf9a510311f0a870c3520d0485ade
smakavan <sachinmakavana@gmail.com>
2025-09-21 11:52:21 +0530

    Update shell_script.sh

--- shell_script.sh
+++ shell_script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-VM1_PUBLIC_IP="$1"
-VM2_PRIVATE_IP="$2"
-SSH_KEY="$3"
+VM1_PUBLIC_IP="4.242.235.115"
+VM2_PRIVATE_IP="10.15.1.4"
+SSH_KEY_PATH="/home/sachin/.ssh/mykeysac"
+SSH_USER="azureuser"
 
-ssh -o StrictHostKeyChecking=no -i $SSH_KEY azureuser@$VM1_PUBLIC_IP "ping -c 4 $VM2_PRIVATE_IP"
+echo "Connecting to VM1"
+ping -c 4 "$VM1_PUBLIC_IP"
+
+echo "Connecting to VM1 via SSH to ping VM2"
+ssh -o StrictHostKeyChecking=no -i "$SSH_KEY_PATH" "$SSH_USER@$VM1_PUBLIC_IP" "ping -c 4 $VM2_PRIVATE_IP"
